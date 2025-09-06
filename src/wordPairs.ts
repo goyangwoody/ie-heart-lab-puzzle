de2d247ea380e6e81b4dc1190c444791e1e8be73
username <modili0102@gmail.com>
2025-09-06 23:34:15 +0900

    fix: support portrait/landscape orientation, update word set

--- src/wordPairs.ts
+++ src/wordPairs.ts
@@ -1,21 +1,17 @@
 // 감정 코드 해독 모듈 - 3027년 H.E.A.R.T 프로젝트
 // 감정 복원을 위한 코드 쌍들
 export const WORD_PAIRS = [
-  { normal: "행복감정", target: "행볻감정" },
-  { normal: "사랑코드", target: "사랑코드" },
-  { normal: "기쁨모듈", target: "기쁨모들" },
-  { normal: "슬픔데이터", target: "슬픔데이타" },
-  { normal: "분노시스템", target: "분노씨스템" },
-  { normal: "평화프로그램", target: "평화프로그랩" },
-  { normal: "희망알고리즘", target: "희망알고리즘" },
-  { normal: "감동네트워크", target: "감동네트웍" },
-  { normal: "설렘변수", target: "설렘변쑤" },
-  { normal: "그리움함수", target: "그리움함쑤" },
-  { normal: "고마움배열", target: "고마움배열" },
-  { normal: "신뢰객체", target: "신뢰갹체" },
-  { normal: "만족루프", target: "만족루프" },
-  { normal: "감격조건", target: "감격조껀" },
-  { normal: "애정클래스", target: "애정클래쓰" }
+  { normal: "훈민정음", target: "휸민정음" },
+  { normal: "사랑해요", target: "사랑헤요" },
+  { normal: "학교종이", target: "핰교종이" },
+  { normal: "서울대학교", target: "서욷대학교" },
+  { normal: "대한민국", target: "대힌민국" },
+  { normal: "파이썬코딩", target: "파이선코딩" },
+  { normal: "컴퓨터공학", target: "컴뮤터공학" },
+  { normal: "인공지능", target: "읻공지능" },
+  { normal: "자연어처리", target: "쟈연어처리" },
+  { normal: "학교에서", target: "학교애서" },
+  { normal: "빅데이터", target: "빅데이타" }
 ]
 
 // You can add more pairs by following this format:
